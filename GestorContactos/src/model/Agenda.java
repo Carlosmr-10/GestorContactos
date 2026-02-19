684f736c9d5cc40efb4a0fb7a159c6660ca9571f
Carlosmr-10 <carlosmichelenar.sanjosemlg@fundacionloyola.net>
2026-02-19 19:57:26 +0100

    metodos creados en agenda

--- GestorContactos/src/model/Agenda.java
+++ GestorContactos/src/model/Agenda.java
@@ -2,8 +2,31 @@ package model;
 
 public class Agenda {
 
-	public Agenda() {
+	private Contactos contacto;
+	
+	public Agenda(Contactos contacto) {
 
+		this.contacto = contacto;
+	}
+	
+	private void agregarContacto(Contactos c) {
+
+		
+	}
+	
+	private void eliminarContacto(String nombre) {
+
+		
+	}
+
+	private boolean buscarContactos(String nombre) {
+
+		boolean existe = false;
+		
+		return existe;
+	}
+	
+	private void listarContactos() {
 		
 	}
 }
