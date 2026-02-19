8c789ab932808c756b69492f40f459f69a5bcd5d
Carlosmr-10 <carlosmichelenar.sanjosemlg@fundacionloyola.net>
2026-02-19 20:13:34 +0100

    metodos de eliminar y buscar contactos implementados

--- GestorContactos/src/model/Agenda.java
+++ GestorContactos/src/model/Agenda.java
@@ -16,12 +16,32 @@ public class Agenda {
 	
 	private void eliminarContacto(String nombre) {
 
-		
+		for (int i = 0; i < listaContactos.size(); i++) {
+			
+			if (nombre.equalsIgnoreCase(listaContactos.get(i).getNombre)) {
+				
+				listaContactos.remove(i);
+			}
+		}
 	}
 
 	private boolean buscarContactos(String nombre) {
 
 		boolean existe = false;
+
+		for (int i = 0; i < listaContactos.size(); i++) {
+			
+			if (nombre.equalsIgnoreCase(listaContactos.get(i).getNombre)) {
+				
+				existe = true;
+				System.out.println("Contacto encontrado");
+				return listaContactos.get(i);
+			}
+			else {
+			
+				System.out.println("Contacto no encontrado");
+			}
+		}
 		
 		return existe;
 	}
